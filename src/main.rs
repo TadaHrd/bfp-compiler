e0955b09501641d2a6b0e7c6dabdde485a08519e
tadaHrd <hrudka.tadeas@gmail.com>
2023-03-01 17:01:11 +0100

    \n escape sequence

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,21 @@ const ERRORS: [&str; 4] = [
 ];
 
 fn main() -> std::io::Result<()> {
-    let args: Vec<String> = std::env::args().collect();
-    
-    if args.len() < 3 {
-        println!("Too little arguments.\nThe correct way to run this is:\n\"bfp <from> <to>\"\n");
-        return Err(std::io::Error::new(std::io::ErrorKind::Other, "Too little arguments"));
-    }
-    
-    let from = std::fs::read_to_string(&args[1])?;
-    let mut to = std::fs::File::create(&args[2])?;
-    
     /*
-    // let from = std::fs::read_to_string("bf.bfp")?;
-    // let mut to = std::fs::File::create("bf.bf")?;
+    // let args: Vec<String> = std::env::args().collect();
+    
+    // if args.len() < 3 {
+    //     println!("Too little arguments.\nThe correct way to run this is:\n\"bfp <from> <to>\"\n");
+    //     return Err(std::io::Error::new(std::io::ErrorKind::Other, "Too little arguments"));
+    // }
+    
+    // let from = std::fs::read_to_string(&args[1])?;
+    // let mut to = std::fs::File::create(&args[2])?;
     */
+    
+    let from = std::fs::read_to_string("bf.bfp")?;
+    let mut to = std::fs::File::create("bf.bf")?;
+    
     use std::io::prelude::*;
     to.write_all(compile_to_bf(from).as_bytes())?;
     
@@ -95,6 +96,11 @@ fn compile_to_bf(input: String) -> String {
                     '\\' | '\"'
                     => {
                         string.push(c);
+                        is_escape_seq = false;
+                    }
+                    
+                    'n' => {
+                        string.push('\n');
                         is_escape_seq = false;
                     }
                     
