cb23c297e8c835afe52e1b351c92921852814e47
tadaHrd <hrudka.tadeas@gmail.com>
2023-03-01 18:33:14 +0100

    Added more compile time errors

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,29 @@
-const ERRORS: [&str; 4] = [
+const ERRORS: [&str; 7] = [
 "Error: Multi line comment did not end",
 "Error: Unterminated String",
 "Error: Empty string",
 "Error: Invalid escape sequence (for '\\' you need \"\\\\\")",
+"Error: More '[' chars than ']'",
+"Error: Fewer '[' chars than ']'",
+"Error: Code is empty",
 ];
 
 fn main() -> std::io::Result<()> {
-    let args: Vec<String> = std::env::args().collect();
+    // // let args: Vec<String> = std::env::args().collect();
     
-    if args.len() < 3 {
-        println!("Too little arguments.\nThe correct way to run this is:\n\"bfp <from> <to>\"\n");
-        return Err(std::io::Error::new(std::io::ErrorKind::Other, "Too little arguments"));
-    }
+    // // if args.len() < 3 {
+    // //     println!("Too little arguments.\nThe correct way to run this is:\n\"bfp <from> <to>\"\n");
+    // //     return Err(std::io::Error::new(std::io::ErrorKind::Other, "Too little arguments"));
+    // // }
     
-    let from = std::fs::read_to_string(&args[1])?;
-    let mut to = std::fs::File::create(&args[2])?;
+    // // let from = std::fs::read_to_string(&args[1])?;
+    // // let mut to = std::fs::File::create(&args[2])?;
     
-    // // let from = std::fs::read_to_string("bf.bfp")?;
-    // // let mut to = std::fs::File::create("bf.bf")?;
+    let from = std::fs::read_to_string("bf.bfp")?;
+    let mut to = std::fs::File::create("bf.bf")?;
     
-    // // use std::io::prelude::*;
-    // // to.write_all(compile_to_bf(from).as_bytes())?;
+    use std::io::prelude::*;
+    to.write_all(compile_to_bf(from).as_bytes())?;
     
     Ok(())
 }
@@ -31,20 +34,32 @@ fn compile_to_bf(input: String) -> String {
     let mut string = String::new();
     let mut keyword = String::new();
     
+    let mut opening_square_bracket_count = 0;
+    let mut closing_square_bracket_count = 0;
+    
     let mut might_comment = false;
     let mut might_end_ml_comment = false;
     let mut is_sl_comment = false;
     let mut is_ml_comment = false;
     let mut is_string = false;
+    let mut is_string_empty = true;
     let mut is_escape_seq = false;
     
     for c in input.chars() {
         if !might_comment && !might_end_ml_comment && !is_sl_comment && !is_ml_comment && !is_string && string == String::new() {
             match c {
                 '>' | '<' | '+' |
-                '-' | '.' | ',' |
-                '[' | ']'
+                '-' | '.' | ','
                 => out.push(c),
+                
+                '[' => {
+                    out.push(c);
+                    opening_square_bracket_count += 1;
+                },
+                ']' => {
+                    out.push(c);
+                    closing_square_bracket_count += 1;
+                }
                 
                 '/' => might_comment = true,
                 '\"' => is_string = true,
@@ -87,19 +102,20 @@ fn compile_to_bf(input: String) -> String {
             }
         }
         else if is_string {
-            let mut is_empty: bool = true;
             if is_escape_seq {
-                is_empty = true;
+                is_string_empty = true;
                 match c {
                     '\\' | '\"'
                     => {
                         string.push(c);
                         is_escape_seq = false;
+                        is_string_empty = false;
                     }
                     
                     'n' => {
                         string.push('\n');
                         is_escape_seq = false;
+                        is_string_empty = false;
                     }
                     
                     _ => {
@@ -112,16 +128,16 @@ fn compile_to_bf(input: String) -> String {
                     '\"' => {
                         is_string = false;
                         keyword = String::new();
-                        if is_empty {
-                            //println!("{}", ERRORS[2]);
-                            //return ERRORS[2].to_string();
+                        if is_string_empty {
+                            println!("{}", ERRORS[2]);
+                            return ERRORS[2].to_string();
                         }
                     },
                     '\\' => is_escape_seq = true,
                     
                     _ => {
                         string.push(c);
-                        is_empty = false;
+                        is_string_empty = false;
                     }
                 }
             }
@@ -136,8 +152,8 @@ fn compile_to_bf(input: String) -> String {
                 }
                 if keyword == "p" {
                     out.push_str(".");
+                    out.push_str("[-]");
                 }
-                out.push_str("[-]");
                 
                 string = String::new();
             }
@@ -150,6 +166,17 @@ fn compile_to_bf(input: String) -> String {
     if is_string {
         println!("{}", ERRORS[1]);
         return ERRORS[1].to_string();
+    }
+    if opening_square_bracket_count > closing_square_bracket_count {
+        println!("{}", ERRORS[4].to_string());
+        return ERRORS[4].to_string();
+    } else if closing_square_bracket_count > opening_square_bracket_count {
+        println!("{}", ERRORS[5].to_string());
+        return ERRORS[5].to_string();
+    }
+    if out.len() == 0 {
+        println!("{}", ERRORS[6].to_string());
+        return ERRORS[6].to_string();
     }
     out
 }
